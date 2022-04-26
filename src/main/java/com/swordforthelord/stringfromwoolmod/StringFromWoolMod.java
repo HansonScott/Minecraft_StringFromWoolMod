edf3b56ad65c64f9dd3d6dc1a06d477a222943ce
Scott Hanson <shanson77@aol.com>
2022-04-25 21:38:21 -0600

    Initial commit

---
+++ src/main/java/com/swordforthelord/stringfromwoolmod/StringFromWoolMod.java
@@ -0,0 +1,9 @@
+package com.swordforthelord.stringfromwoolmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(StringFromWoolMod.MOD_ID)
+
+public class StringFromWoolMod {
+    public static final String MOD_ID = "stringfromwoolmod";
+}
